88c1b7ea075c233b14bb4014086aad6b98c0e40c
Peter Pesotskiy <ppeter777@yandex.ru>
2023-07-25 23:41:34 +0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    application
+}
+
+application {
+    mainClass.set("org.example.hexlet.HelloWorld")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Версии зависимостей могут отличаться
+    // Здесь мы сразу подключаем зависимости,
+    // которые понадобятся во время обучения
+    implementation("io.javalin:javalin:5.6.1")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("io.javalin:javalin-rendering:5.6.0")
+    implementation("gg.jte:jte:3.0.1")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
